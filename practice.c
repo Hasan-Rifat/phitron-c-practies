36eea2b9cd10e02ebd81a3c2c01d7f5c8de2b625
Hasan-Rifat <developerhr000@gmail.com>
2024-05-23 21:45:30 +0600

    first commit

---
+++ practice.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hi there /\\. %%I hope you are doing great and practicing well. \n Now this is a simple task you need to do.\n Just care for the indentation.\n Show 100%% sincerity.");
+    return 0;
+}
