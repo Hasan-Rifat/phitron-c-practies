e546225d9b6dbfe1c6b14b3c70c4fb2e6cf32ba6
Hasan-Rifat <developerhr000@gmail.com>
2024-05-26 02:20:50 +0600

    chore: Update loop.c to calculate Collatz sequence

--- loop.c
+++ loop.c
@@ -5,17 +5,43 @@
 
 int main()
 {
-        int i = 0;
-        while (i != 10)
+        // int i = 0;
+        // while (i != 10)
+        // {
+        //         printf("Value of i is %d\n", i);
+        //         i++;
+        // }
+
+        int n;
+        printf("Enter a number: ");
+        scanf("%d", &n);
+
+        for (int i = n; i > 1; i--)
         {
-                printf("Value of i is %d\n", i);
-                i++;
+                printf("---->%d\n", n);
+                if (n % 2 == 0)
+                {
+                        n = n / 2;
+                }
+                else
+                {
+                        n = 3 * n + 1;
+                }
         }
 
-        for (int j = 0; j < 10; j++)
-        {
-                printf("Value of j is %d\n", j);
-        }
+        // while (n > 1)
+        // {
+
+        //         printf("---->%d\n", n);
+        //         if (n % 2 == 0)
+        //         {
+        //                 n = n / 2;
+        //         }
+        //         else
+        //         {
+        //                 n = 3 * n + 1;
+        //         }
+        // }
 
         return 0;
 }
