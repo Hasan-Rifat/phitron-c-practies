39c696931616da10ffeff9c49901109f0813d339
Hasan-Rifat <developerhr000@gmail.com>
2024-05-29 01:24:28 +0600

    Refactor CSESProblemSet.c to remove unnecessary code

---
+++ problem2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int s;
+    if (n % 5 == 0)
+    {
+        s = n / 5;
+    }
+    else
+    {
+        s = (n / 5) + 1;
+    }
+    return 0;
+}
