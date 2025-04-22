d20bd6a64963db4ca3d532ee6e5bc063c92ca5df
Hasan-Rifat <developerhr000@gmail.com>
2025-04-22 18:48:29 +0600

    update m-7.9 added arrary reverse

---
+++ c/precties/week3/array_input_outout.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int ar[5];
+
+    scanf("%d %d %d %d %d", &ar[0], &ar[1], &ar[2], &ar[3], &ar[4]);
+    printf("%d %d %d %d %d", ar[0], ar[1], ar[2], ar[3], ar[4]);
+    return 0;
+}
