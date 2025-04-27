b30a8bacdcb8a81b595d6d9d6096fd5f37a2b8cf
Hasan-Rifat <developerhr000@gmail.com>
2025-04-27 10:51:50 +0600

    Add string initialization example in string_initialization.c
    - Created a new source file string_initialization.c
    - Initialized a character array with the string "Hasan Rifat"
    - Added a printf statement to output the initialized string
    - Commented out an alternative character array initialization for reference

---
+++ c/precties/week3/module-10/string_initialization.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    // char s[4] = {
+    //     'P',
+    //     'I',
+    //     'A',
+    //     'S',
+    // };
+    char s[30] = "Hasan Rifat";
+    printf("%s\n", s); // This will print "PIAS" without a null terminator
+    return 0;
+}
