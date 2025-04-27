df1c1f84c5b4a89bbf00af313dc4ba1596eee8b2
Hasan-Rifat <developerhr000@gmail.com>
2025-04-27 10:34:48 +0600

    Add initial implementation of test program
    - Created a new C source file `test.c` that includes a simple program.
    - The program reads a character input from the user and prints it to the console.
    - Utilizes standard input/output functions from the `stdio.h` library.

---
+++ c/precties/week3/module-10/string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char a[5];
+    scanf("%s", a);
+    printf("%s\n", a);
+
+    return 0;
+}
---
+++ c/precties/week3/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[20];
+
+    scanf("%c", &str);
+    printf("%s", str);
+    return 0;
+}
